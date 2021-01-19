a11d9cd3aaa044129f8e25329ef67201a1086560
Kevin Ivan Sanchez Valdin <62733314+KevinSanchez-Cat@users.noreply.github.com>
2021-01-18 23:07:36 -0600

    Pedidos y envios

--- Controllers/Orden_clienteController.cs
+++ Controllers/Orden_clienteController.cs
@@ -10,7 +10,6 @@ using TiendaMotores.Models;
 
 namespace TiendaMotores.Controllers
 {
-    [Authorize]
     public class Orden_clienteController : Controller
     {
         private TIENDAEntities db = new TIENDAEntities();
@@ -19,6 +18,16 @@ namespace TiendaMotores.Controllers
         public ActionResult Index()
         {
             var oden_cliente = db.Oden_cliente.Include(o => o.Compra).Include(o => o.Paqueteria);
+            return View(oden_cliente.ToList());
+        }
+        public ActionResult ModificarFechaEnvio()
+        {
+            var oden_cliente =db.Oden_cliente.Where(o=>o.fecha_envio==null).OrderBy(o=>o.fecha_creacion).Include(o => o.Compra).Include(o => o.Paqueteria);
+            return View(oden_cliente.ToList());
+        }
+        public ActionResult ModificarFechaEntrega()
+        {
+            var oden_cliente = db.Oden_cliente.Where(o => o.fecha_entrega == null && o.fecha_envio!=null).OrderBy(o => o.fecha_creacion).Include(o => o.Compra).Include(o => o.Paqueteria);
             return View(oden_cliente.ToList());
         }
 
@@ -81,16 +90,60 @@ namespace TiendaMotores.Controllers
             return View(oden_cliente);
         }
 
+        // GET: Orden_cliente/Edit/5
+        public ActionResult Edit1(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Oden_cliente oden_cliente = db.Oden_cliente.Find(id);
+            if (oden_cliente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_compra = new SelectList(db.Compra, "id_compra", "id_compra", oden_cliente.id_compra);
+            ViewBag.id_paqueteria = new SelectList(db.Paqueteria, "id_paqueteria", "nombre_paqueteria", oden_cliente.id_paqueteria);
+            return View(oden_cliente);
+        }
+
+
+
         // POST: Orden_cliente/Edit/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
         // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id_orden,id_compra,num_comfirmacion,fecha_creacion,total,num_serie,fecha_envio,fecha_entrega,id_paqueteria")] Oden_cliente oden_cliente)
+        public ActionResult Edit([Bind(Include = "Id_orden,num_serie,fecha_envio,id_paqueteria")] Oden_cliente oden_cliente)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(oden_cliente).State = EntityState.Modified;
+                Oden_cliente o = db.Oden_cliente.Find(oden_cliente.Id_orden);
+                o.id_paqueteria = oden_cliente.id_paqueteria;
+                o.num_serie = oden_cliente.num_serie;
+                o.fecha_envio = oden_cliente.fecha_envio;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_compra = new SelectList(db.Compra, "id_compra", "id_compra", oden_cliente.id_compra);
+            ViewBag.id_paqueteria = new SelectList(db.Paqueteria, "id_paqueteria", "nombre_paqueteria", oden_cliente.id_paqueteria);
+            return View(oden_cliente);
+        }
+
+
+
+        // POST: Orden_cliente/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit1([Bind(Include = "Id_orden,fecha_entrega,id_paqueteria")] Oden_cliente oden_cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                Oden_cliente o = db.Oden_cliente.Find(oden_cliente.Id_orden);
+                o.fecha_entrega = oden_cliente.fecha_entrega;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -133,28 +186,5 @@ namespace TiendaMotores.Controllers
             }
             base.Dispose(disposing);
         }
-        public ActionResult Eliminar()
-        {
-            return View();
-        }
-        public ActionResult Modificar()
-        {
-            return View();
-        }
-
-        public ActionResult Ver()
-        {
-            return View();
-        }
-
-        public ActionResult Desplegar()
-        {
-            return View();
-        }
-        public ActionResult Buscar()
-        {
-            return View();
-        }
     }
-
 }
